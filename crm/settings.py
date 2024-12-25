f8407fdc6502cf1a7a8f7096105531aab6d3ca5d
Yagnesh Gohil <ypgohil2002@gmail.com>
2024-12-25 17:34:17 +0530

    settings fixed

--- crm/settings.py
+++ crm/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydatabase',
         'USER': 'root',
-        'PASSWORD': 'YgohilEE@21',
+        'PASSWORD': 'pwd',
         'HOST': 'localhost',
         'PORT': '3306',
     }
