b2b30495a91820e2670bbe64ee6674cfb19b43e0
Yagnesh Gohil <ypgohil2002@gmail.com>
2024-12-28 00:42:57 +0530

    CRUD operations working fine...

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -54,4 +54,51 @@ def register_user(request):
     return render(request, 'register.html', {'form':form})
   
   return render(request, 'register.html', {'form':form})
+
+
+def individual_record(request, pk):
+  if request.user.is_authenticated:
+    record = Record.objects.get(id=pk)
+    return render(request, 'individual_record.html', {'record':record})
+  else:
+    messages.success(request, "You cannot see the page, login first to see the content of the page!")
+    return redirect('home')
+  
+def delete_record(request, pk):
+  if request.user.is_authenticated:
+    delete_rec = Record.objects.get(id=pk)
+    delete_rec.delete()
+    messages.success(request, "Record deleted successfully!")
+    return redirect('home')
+  else:
+    messages.success(request, "You cannot see the page, login first to see the content of the page!")
+    return redirect('home')
+
+def add_record(request):
+  form = AddRecordForm(request.POST or None)
+  if request.user.is_authenticated:
+    if request.method == "POST":
+      if form.is_valid():
+        form.save()
+        messages.success(request, "Record added successfully!")
+        return redirect('home')
+    return render(request, 'add_record.html', {'form':form})
+  else:
+    messages.success(request, "You cannot access the page, login first to see the content of the page!")
+    return redirect('home')
+  
+def update_record(request, pk):
+  if request.user.is_authenticated:
+    current_data = Record.objects.get(id=pk)
+    form = AddRecordForm(request.POST or None, instance=current_data)
+    if request.method == "POST":
+      if form.is_valid():
+        form.save()
+        messages.success(request, "Record Updated successfully!")
+        return redirect('home')
+    else:
+      return render(request, 'update_record.html', {'form':form})
+  else:
+    messages.success(request, "You cannot access the page, login first to see the content of the page!")
+    return redirect('home')
   
